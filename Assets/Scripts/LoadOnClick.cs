b6555d674cea3b6534c6804e67173403646fa79b
Siv <chevvuris@gmail.com>
2018-04-27 16:29:05 -0700

    imported titlescreen package

---
+++ Assets/Scripts/LoadOnClick.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadOnClick : MonoBehaviour {
+	public GameObject loadingImage;
+
+	public void loadScene(int level)
+	{
+		//loading picture
+		loadingImage.SetActive (true);
+		//used to load scenes in Unity
+		//level parameter = index in build settings of level we want to load
+		Application.LoadLevel(level);
+	}
+}
